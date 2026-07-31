b55ee939f8a0d0082f3c173e919ceb030aa387f5
Sundar Shrestha <sundarstha469@gmail.com>
2026-07-31 10:24:03 +0545

    fix: reject passwords containing the username as a substring

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,13 +1,37 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError as DjangoValidationError
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 
 class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        validators=[UniqueValidator(
+            queryset=User.objects.all(),
+            message="A user with this email already exists."
+        )]
+    )
     password = serializers.CharField(write_only=True, min_length=8)
 
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
+
+    def validate_password(self, value):
+        username = self.initial_data.get('username', '')
+        if username and username.lower() in value.lower():
+            raise serializers.ValidationError("Password cannot contain your username.")
+
+        temp_user = User(
+            username=self.initial_data.get('username', ''),
+            email=self.initial_data.get('email', ''),
+        )
+        try:
+            validate_password(value, user=temp_user)
+        except DjangoValidationError as e:
+            raise serializers.ValidationError(list(e.messages))
+        return value
 
     def create(self, validated_data):
         return User.objects.create_user(
